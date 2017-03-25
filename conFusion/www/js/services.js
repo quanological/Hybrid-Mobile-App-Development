0801370bacc951f579f2ed0c5a710054d86ca1fb
quanological <qmtruong92@gmail.com>
2017-03-25 14:18:32 -0700

    [working] Week 2 Assignment - Before refactoring of getDishes function

--- conFusion/www/js/services.js
+++ conFusion/www/js/services.js
@@ -1,46 +1,32 @@
 'use strict';
 
-angular.module('conFusion.services' , ['ngResource'])
-        .constant("baseURL","http://localhost:3000/")
-        .service('menuFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+angular.module('conFusion.services', ['ngResource'])
+  .constant("baseURL", "http://localhost:3000/")
+  .service('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
-            var promotions = [
-                {
-                          _id:0,
-                          name:'Weekend Grand Buffet',
-                          image: 'images/buffet.png',
-                          label:'New',
-                          price:'19.99',
-                          description:'Featuring mouthwatering combinations with a choice of five different salads, six enticing appetizers, six main entrees and five choicest desserts. Free flowing bubbly and soft drinks. All for just $19.99 per person ',
-                }
+    this.getDishes = function () {
 
-            ];
+      return $resource(baseURL + "dishes/:id", null, {'update': {method: 'PUT'}});
 
-                this.getDishes = function(){
-
-                    return $resource(baseURL+"dishes/:id",null,  {'update':{method:'PUT' }});
-
-                };
-
-                // implement a function named getPromotion
-                // that returns a selected promotion.
-                this.getPromotion = function() {
-                    return   $resource(baseURL+"promotions/:id");;
-                }
+    };
 
 
-        }])
+  }])
 
-        .factory('corporateFactory', ['$resource', 'baseURL', function($resource,baseURL) {
-            return $resource(baseURL+"leadership/:id");
-        }])
+  .factory('promotionFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+    return $resource(baseURL + "promotions/:id");
+  }])
 
-        .factory('feedbackFactory', ['$resource', 'baseURL', function($resource,baseURL) {
-            return $resource(baseURL+"feedback/:id");
+  .factory('corporateFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+    return $resource(baseURL + "leadership/:id");
+  }])
 
-        }])
+  .factory('feedbackFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+    return $resource(baseURL + "feedback/:id");
 
-  .factory('favoriteFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+  }])
+
+  .factory('favoriteFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
     var favFac = {};
     var favorites = [];
@@ -58,7 +44,7 @@ angular.module('conFusion.services' , ['ngResource'])
 
     // return the favorites object
     // is also supplied to the favoritesFilter
-    favFac.getFavorites = function() {
+    favFac.getFavorites = function () {
       return favorites;
     };
 
@@ -71,7 +57,6 @@ angular.module('conFusion.services' , ['ngResource'])
         }
       }
     };
-
 
 
     return favFac;
