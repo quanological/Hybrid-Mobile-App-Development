517e2a466f867a8fbc348c3d8a60b7fb722645fc
quanological <qmtruong92@gmail.com>
2017-03-24 21:20:09 -0700

    Week 2 Ionic Lists Advanced Features

--- conFusion/www/js/controllers.js
+++ conFusion/www/js/controllers.js
@@ -39,22 +39,22 @@ angular.module('conFusion.controllers', [])
     // Create the reserve modal that we will use later
     $ionicModal.fromTemplateUrl('templates/reserve.html', {
       scope: $scope
-    }).then(function(modal) {
+    }).then(function (modal) {
       $scope.reserveform = modal;
     });
 
     // Triggered in the reserve modal to close it
-    $scope.closeReserve = function() {
+    $scope.closeReserve = function () {
       $scope.reserveform.hide();
     };
 
     // Open the reserve modal
-    $scope.reserve = function() {
+    $scope.reserve = function () {
       $scope.reserveform.show();
     };
 
     // Perform the reserve action when the user submits the reserve form
-    $scope.doReserve = function() {
+    $scope.doReserve = function () {
       console.log('Doing reservation', $scope.reservation);
 
       // Simulate a reservation delay. Remove this and replace with your reservation
@@ -65,50 +65,59 @@ angular.module('conFusion.controllers', [])
     };
   })
 
-  .controller('MenuController', ['$scope', 'menuFactory', 'baseURL', function ($scope, menuFactory, baseURL) {
+  .controller('MenuController', ['$scope', 'menuFactory', 'favoriteFactory', '$ionicListDelegate',
+    'baseURL', function ($scope, menuFactory, favoriteFactory, $ionicListDelegate, baseURL) {
 
-    $scope.baseURL = baseURL;
-    $scope.tab = 1;
-    $scope.filtText = '';
-    $scope.showDetails = false;
-    $scope.showMenu = false;
-    $scope.message = "Loading ...";
+      $scope.baseURL = baseURL;
+      $scope.tab = 1;
+      $scope.filtText = '';
+      $scope.showDetails = false;
+      $scope.showMenu = false;
+      $scope.message = "Loading ...";
 
-    menuFactory.getDishes().query(
-      function (response) {
-        $scope.dishes = response;
-        $scope.showMenu = true;
-      },
-      function (response) {
-        $scope.message = "Error: " + response.status + " " + response.statusText;
-      });
+      menuFactory.getDishes().query(
+        function (response) {
+          $scope.dishes = response;
+          $scope.showMenu = true;
+        },
+        function (response) {
+          $scope.message = "Error: " + response.status + " " + response.statusText;
+        });
 
 
-    $scope.select = function (setTab) {
-      $scope.tab = setTab;
+      $scope.select = function (setTab) {
+        $scope.tab = setTab;
 
-      if (setTab === 2) {
-        $scope.filtText = "appetizer";
+        if (setTab === 2) {
+          $scope.filtText = "appetizer";
+        }
+        else if (setTab === 3) {
+          $scope.filtText = "mains";
+        }
+        else if (setTab === 4) {
+          $scope.filtText = "dessert";
+        }
+        else {
+          $scope.filtText = "";
+        }
+      };
+
+      $scope.isSelected = function (checkTab) {
+        return ($scope.tab === checkTab);
+      };
+
+      $scope.toggleDetails = function () {
+        $scope.showDetails = !$scope.showDetails;
+      };
+
+
+      // adds a favorite dish to My Favorites
+      $scope.addFavorite = function (index) {
+        console.log("index is " + index);
+        favoriteFactory.addToFavorites(index);
+        $ionicListDelegate.closeOptionButtons();
       }
-      else if (setTab === 3) {
-        $scope.filtText = "mains";
-      }
-      else if (setTab === 4) {
-        $scope.filtText = "dessert";
-      }
-      else {
-        $scope.filtText = "";
-      }
-    };
-
-    $scope.isSelected = function (checkTab) {
-      return ($scope.tab === checkTab);
-    };
-
-    $scope.toggleDetails = function () {
-      $scope.showDetails = !$scope.showDetails;
-    };
-  }])
+    }])
 
   .controller('ContactController', ['$scope', function ($scope) {
 
@@ -209,5 +218,51 @@ angular.module('conFusion.controllers', [])
     console.log($scope.leaders);
 
   }])
+
+  .controller('FavoritesController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function ($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
+
+    $scope.baseURL = baseURL;
+    $scope.shouldShowDelete = false;
+
+    $scope.favorites = favoriteFactory.getFavorites();
+
+    $scope.dishes = menuFactory.getDishes().query(
+      function (response) {
+        $scope.dishes = response;
+      },
+      function (response) {
+        $scope.message = "Error: " + response.status + " " + response.statusText;
+      });
+    console.log($scope.dishes, $scope.favorites);
+
+    $scope.toggleDelete = function () {
+      $scope.shouldShowDelete = !$scope.shouldShowDelete;
+      console.log($scope.shouldShowDelete);
+    };
+
+    $scope.deleteFavorite = function (index) {
+
+      favoriteFactory.deleteFromFavorites(index);
+      $scope.shouldShowDelete = false;
+
+    }
+  }])
+
+
+  //returns the favorite dishes
+  .filter('favoriteFilter', function () {
+    return function (dishes, favorites) {
+      var out = [];
+      for (var i = 0; i < favorites.length; i++) {
+        for (var j = 0; j < dishes.length; j++) {
+          if (dishes[j].id === favorites[i].id)
+            out.push(dishes[j]);
+        }
+      }
+      return out;
+
+    }
+  });
+
 
 ;
--- conFusion/www/js/services.js
+++ conFusion/www/js/services.js
@@ -32,17 +32,50 @@ angular.module('conFusion.services' , ['ngResource'])
         }])
 
         .factory('corporateFactory', ['$resource', 'baseURL', function($resource,baseURL) {
-
-
             return $resource(baseURL+"leadership/:id");
-
         }])
 
         .factory('feedbackFactory', ['$resource', 'baseURL', function($resource,baseURL) {
-
-
             return $resource(baseURL+"feedback/:id");
 
         }])
+
+  .factory('favoriteFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+
+    var favFac = {};
+    var favorites = [];
+
+    // add to favorites object
+    favFac.addToFavorites = function (index) {
+      for (var i = 0; i < favorites.length; i++) {
+        if (favorites[i].id == index)
+          return;
+
+      }
+      favorites.push({id: index}); // pushes by id
+    };
+
+
+    // return the favorites object
+    // is also supplied to the favoritesFilter
+    favFac.getFavorites = function() {
+      return favorites;
+    };
+
+
+    // splice out from favorites if the favorite is found in the object
+    favFac.deleteFromFavorites = function (index) {
+      for (var i = 0; i < favorites.length; i++) {
+        if (favorites[i].id == index) {
+          favorites.splice(i, 1);
+        }
+      }
+    };
+
+
+
+    return favFac;
+
+  }])
 
 ;
